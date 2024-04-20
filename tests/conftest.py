dabdedc77185e145c432d1b3ad5a002df232c6fc
Alexander Molofeev <alexandr.nino@gmail.com>
2024-04-20 08:39:53 +0300

    clean up conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,33 +4,25 @@ from alembic.command import downgrade as alembic_downgrade
 from alembic.command import upgrade as alembic_upgrade
 from alembic.config import Config as AlembicConfig
 from fastapi.testclient import TestClient
-from sqlalchemy.ext.asyncio import AsyncEngine
 
 from src.app import app
 from src.di.container import init_container
 
 
 @pytest.fixture
-async def pg_engine(event_loop, container):  # pylint: disable=unused-argument
+async def migrations(event_loop, container):  # pylint: disable=unused-argument
     """fixture for migrations"""
 
     config = AlembicConfig('alembic.ini')
     alembic_upgrade(config, 'head')
 
-    yield container._connection_pool
+    yield
 
     alembic_downgrade(config, 'base')
 
 
 @pytest.fixture
-async def db_connection(pg_engine: AsyncEngine):
-    """fixture for connection"""
-    async with pg_engine.begin() as conn:
-        yield conn
-
-
-@pytest.fixture
-async def application(pg_engine, container):  # pylint: disable=W0613
+async def application(migrations, container):  # pylint: disable=W0613
     """override dependencies"""
     yield app
 
