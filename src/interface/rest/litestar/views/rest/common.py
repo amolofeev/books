2caa698d5b4fe7a28539662d5d3941914f372474
Alexander Molofeev <alexandr.nino@gmail.com>
2024-09-25 22:08:43 +0300

    add delay multiplier into test endpoints

--- src/interface/rest/litestar/views/rest/common.py
+++ src/interface/rest/litestar/views/rest/common.py
@@ -13,32 +13,32 @@ class TestHandler(Controller):
     async def a_test(self) -> None:
         await blahblah()
 
-    @get(path="/get/{status_code:int}/{latency:float}")
-    async def get(self, status_code: int, latency: float) -> Response:
-        if random.random() <= latency:
-            await asyncio.sleep(random.random())
+    @get(path="/get/{status_code:int}/{slow_percent:float}/{latency_multiply:float}")
+    async def get(self, status_code: int, slow_percent: float, latency_multiply: float) -> Response:
+        if random.random() <= slow_percent:
+            await asyncio.sleep(random.random() * latency_multiply)
         return Response(None, status_code=status_code)
 
-    @post(path="/post/{status_code:int}/{latency:float}")
-    async def post(self, status_code: int, latency: float) -> Response:
-        if random.random() <= latency:
-            await asyncio.sleep(random.random())
+    @post(path="/post/{status_code:int}/{slow_percent:float}/{latency_multiply:float}")
+    async def post(self, status_code: int, slow_percent: float, latency_multiply: float) -> Response:
+        if random.random() <= slow_percent:
+            await asyncio.sleep(random.random() * latency_multiply)
         return Response(None, status_code=status_code)
 
-    @put(path="/put/{status_code:int}/{latency:float}")
-    async def put(self, status_code: int, latency: float) -> Response:
-        if random.random() <= latency:
-            await asyncio.sleep(random.random())
+    @put(path="/put/{status_code:int}/{slow_percent:float}/{latency_multiply:float}")
+    async def put(self, status_code: int, slow_percent: float, latency_multiply: float) -> Response:
+        if random.random() <= slow_percent:
+            await asyncio.sleep(random.random() * latency_multiply)
         return Response(None, status_code=status_code)
 
-    @patch(path="/patch/{status_code:int}/{latency:float}")
-    async def patch(self, status_code: int, latency: float) -> Response:
-        if random.random() <= latency:
-            await asyncio.sleep(random.random())
+    @patch(path="/patch/{status_code:int}/{slow_percent:float}/{latency_multiply:float}")
+    async def patch(self, status_code: int, slow_percent: float, latency_multiply: float) -> Response:
+        if random.random() <= slow_percent:
+            await asyncio.sleep(random.random() * latency_multiply)
         return Response(None, status_code=status_code)
 
-    @delete(path="/delete/{status_code:int}/{latency:float}", status_code=204)
-    async def delete(self, status_code: int, latency: float) -> None:
-        if random.random() <= latency:
-            await asyncio.sleep(random.random())
+    @delete(path="/delete/{status_code:int}/{slow_percent:float}/{latency_multiply:float}", status_code=204)
+    async def delete(self, status_code: int, slow_percent: float, latency_multiply: float) -> None:
+        if random.random() <= slow_percent:
+            await asyncio.sleep(random.random() * latency_multiply)
         return Response(None, status_code=status_code)
